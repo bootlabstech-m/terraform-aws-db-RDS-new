e5bcc1fef9d21384c2a1ca88fb6bfad75397aff4
Bhargava Naidu <bhargava@bootlabstech.com>
2026-01-08 16:10:01 +0530

    provider to previous version

--- provider.tf
+++ provider.tf
@@ -1,19 +1,12 @@
 terraform {
-  required_version = ">= 0.13" # previous CLI version ">= 1.11.4"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.46.0" # Please check the official doc of terraform/aws before upgrading
-    }
-  }
-
+  required_version = ">= 0.13"
   backend "s3" {}
 }
 
 provider "aws" {
-  region = var.region
+  # Configuration options 
   assume_role {
     role_arn = var.role_arn
   }
+  region = var.region
 }
